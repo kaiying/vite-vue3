62241773155f8b00641823974de7aa5ffe2c4502
KathyLai <kaiying.lai@gmail.com>
2021-04-06 15:31:51 +0800

    vue router

---
+++ src/router.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import('@/src/pages/home.vue'),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import('@/src/pages/about.vue'),
+  },
+];
+
+export default createRouter({
+  history: createWebHistory('/'),
+  routes,
+});
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+
+console.log(resolve(__dirname, ''))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, ''),
+    },
+  }
 })
