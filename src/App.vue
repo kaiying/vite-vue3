4d027b3c1013ad96a11c7041a2950b7a02cfb53b
KathyLai <kaiying.lai@gmail.com>
2021-04-06 16:37:59 +0800

    using env constant sample

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <hr/>
+  <h1>{{text}}</h1>
+  <hr/>
   <router-view />
   <router-link to="/about">
     about
@@ -9,6 +12,7 @@
 </template>
 
 <script setup>
+const text = import.meta.env.VITE_API_HOST;
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
