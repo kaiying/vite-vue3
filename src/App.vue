36cfd1ea4bb420b9856d7c35299c4b528cf0ac8e
KathyLai <kaiying.lai@gmail.com>
2021-04-06 17:30:13 +0800

    add axios package

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
-h1 {{text}}
+h1 Axios
+h2 {{url}}
+h2 {{res()}}
 router-link(to="/about") about
 router-link(to="/home") index
 </template>
 
 <script setup>
+import axios from '@/src/services/axios'
+
+const url = `${import.meta.env.VITE_API_HOST}${import.meta.env.VITE_API_POSTS}`;
+
+const res = async () => await axios({
+  method: 'get',
+  url: url,
+}).then(res => res.data);
 const text = import.meta.env.VITE_API_HOST;
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
