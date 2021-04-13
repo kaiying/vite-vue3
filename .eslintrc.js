e8258b9bad58ac418c17ff6ead4adebe7c745cbc
KathyLai <kaiying.lai@gmail.com>
2021-04-13 11:38:53 +0800

    add code smell tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,39 @@
 module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   extends: [
     // add more generic rulesets here, such as:
     // "eslint:recommended",
     "plugin:vue/vue3-recommended",
+    'airbnb-base',
+    'plugin:prettier/recommended'
     // "plugin:vue/recommended" // Use this if you are using Vue.js 2.x.
   ],
+  plugins: ['vue'],
   rules: {
     // override/add rules settings here, such as:
     "vue/no-multiple-template-root": "off",
-
+    'prettier/prettier': 'error',
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'global-require': 'off',
+    'no-shadow': 'off',
+    'no-param-reassign': 'off',
+    'func-names': 'off',
+    'import/prefer-default-export': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'import/extensions': 'off',
+    'no-return-assign': 'off',
   },
 }
