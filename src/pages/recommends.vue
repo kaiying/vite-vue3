a4d2fcf5e1c2f733cca5612465fa691dd146ba9f
KathyLai <kaiying.lai@gmail.com>
2021-04-15 16:27:17 +0800

    update recommends.vue : add fake data (test using)

--- src/pages/recommends.vue
+++ src/pages/recommends.vue
@@ -4,20 +4,28 @@
   h3 {{ undefined || 'none'}}
   span v-model
   h3 {{ searchKeywords }}
+  //div
+    span is ?
+    h4 {{recommendList}}
 .row.col-6
   form.d-flex.w-100
     input.form-control.w-75(v-model="searchKeywords")
     button.btn.btn-secondary -x-) |||
-auto-complete-list
+auto-complete-list(:list="recommendList")
 </template>
 
 <script>
 import { ref } from 'vue';
 import AutoCompleteList from '@/src/components/search/auto-complete-list.vue';
 
+const fakeResponse = ['貓貓蟲咖波', '貓貓蟲咖波: 奇幻太空之旅', '貓貓蟲咖波: 小萌物到處玩', '貓貓蟲咖波: 暖暖春天文具組, 陪你慵懶每一天', '貓貓蟲咖波: 縮小世界大冒險', '貓貓蟲咖波: 食物世界超棒'];
+
 const setup = function () {
   const searchKeywords = ref('');
-  return { searchKeywords };
+  return {
+    searchKeywords,
+    recommendList: fakeResponse,
+  };
 };
 
 export default {
