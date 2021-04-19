83d2981f0e0e24d703243f91b9e89657f311e92d
KathyLai <kaiying.lai@gmail.com>
2021-04-19 11:53:00 +0800

    Create auto-complete.js

---
+++ src/constant/search/auto-complete.js
@@ -0,0 +1,23 @@
+import { values } from 'ramda';
+
+export const autoCompleteCount = {
+  mobile: 3,
+  history: 5,
+  recommends: 15,
+};
+
+export const arrowKeyEnum = {
+  up: 'ArrowUp',
+  down: 'ArrowDown',
+  left: 'ArrowLeft',
+  right: 'ArrowRight',
+};
+
+export const specialKeyEnum = {
+  enter: 'Enter',
+  esc: 'Escape',
+};
+
+export const specialKeys = [...values(arrowKeyEnum), specialKeyEnum.enter];
+
+export const hoverDefaultIndex = -1;
