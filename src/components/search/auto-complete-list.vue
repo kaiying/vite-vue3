d8b70ffd246c0aa771c8418e57050354d7e9080b
KathyLai <kaiying.lai@gmail.com>
2021-04-19 10:30:58 +0800

    feat: props pass to child components (setup style)

--- src/components/search/auto-complete-list.vue
+++ src/components/search/auto-complete-list.vue
@@ -1,11 +1,44 @@
 <template lang="pug">
 .list
-  .item
+  .item(v-for="(item, index) in items")
+    span {{item.keyword}}
+    span.font-weight-bold {{item.otherWords}}
 </template>
 
 <script>
+import { toRefs, computed } from 'vue';
+import { splitAt, map, length, pipe } from 'ramda';
+
+const items = ({ list, keyword }) =>
+  computed(() => {
+    return pipe(
+      // reset keywords format
+      map((x) => splitAt(length(keyword.value))(x)),
+      map((x) => ({ keyword: x[0], otherWords: x[1] })),
+    )(list.value);
+  });
+
+const props = {
+  list: {
+    type: Array,
+    default: [],
+  },
+  keyword: {
+    type: String,
+    default: '',
+  },
+};
+
+const setup = function (props) {
+  const { list, keyword } = toRefs(props);
+  return {
+    items: items({ list, keyword }),
+  };
+};
 export default {
   name: 'AutoCompleteList',
+  props,
+  setup,
 };
 </script>
 
