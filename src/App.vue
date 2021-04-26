bf1f3ef925bcf656c51e080593347b76dd3dba3e
KathyLai <kaiying.lai@gmail.com>
2021-04-26 15:15:36 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template lang="pug">
 .container
-  h1 Axios
-  h2 {{url}}
-  h2 {{res()}}
-  hr
-  router-view
-  hr
-  router-link(to="/about") about
-  router-link(to="/home") index
-  router-link(to="/recommends") recommends
+  .d-flex.justify-content-around.bg-light.py-3
+    router-link(to="/about") about
+    router-link(to="/home") index
+    router-link(to="/recommends") recommends
+  div.mt-2
+    router-view
 </template>
 
 <script setup>
